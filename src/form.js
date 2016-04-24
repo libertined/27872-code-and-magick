9a21ad56f1e85dcb08ba49ba865c52aff2c37d29
Diana <diana_box@list.ru>
2016-04-24 17:35:31 +0300

    Save in values using cookies

--- src/form.js
+++ src/form.js
@@ -4,6 +4,10 @@
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
+  var reviewForm = document.querySelector('form.review-form');
+  var nameField = document.querySelector('#review-name');
+  var markField = document.getElementsByName('review-mark');
+  var browserCookies = require('browser-cookies');
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
@@ -14,4 +18,38 @@
     evt.preventDefault();
     formContainer.classList.add('invisible');
   };
+
+  reviewForm.onsubmit = function(evt) {
+    evt.preventDefault();
+
+    //Определяем текущую дату
+    var today = new Date();
+    var myBidth = new Date(today.getFullYear() + '-04-28');
+    if(myBidth.valueOf() > today.valueOf()) {
+      myBidth = new Date(today.getFullYear() - 1 + '-04-28');
+    }
+    var dateToCookies = today.valueOf() - myBidth.valueOf();
+
+    var curMark = '';
+    for(var j = 0; j < markField.length; j++) {
+      if (markField[j].checked) {
+        curMark = markField[j].value;
+      }
+    }
+
+    browserCookies.set('mark', curMark, {
+      expires: today.valueOf + dateToCookies
+    });
+    browserCookies.set('name', nameField.value, {
+      expires: today.valueOf + dateToCookies
+    });
+
+    this.submit();
+  }
+
+  //Первоначальные настройки
+  nameField.value = browserCookies.get('name') || '';
+  var defMark = browserCookies.get('mark') || 3;
+  document.getElementById('review-mark-' + defMark).checked = true;
+
 })();
